c12e93c578623a455cc75288e7b5cb621648e3cd
Manthan29-code <141537615+Manthan29-code@users.noreply.github.com>
2024-03-09 08:54:10 +0530

    Update starpattern.py

--- starpattern.py
+++ starpattern.py
@@ -4,8 +4,9 @@ for i in range(65,71):
     for j in range(65,i):
           print(chr(j),end='')
     print("")    
-             
-print("THE END")        
+print("That's it in this file")             
+print("THE END")
+
 
 
 
